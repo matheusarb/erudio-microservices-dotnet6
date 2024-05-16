e5904d9d47adf0e93a883ba5504ecded93e84250
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-05-16 11:00:51 -0300

    feat: adding interface and implementation of services

---
+++ Rest.ASPNET/Rest.ASPNET.API/Services/IPersonService.cs
@@ -0,0 +1,13 @@
+﻿using Rest.ASPNET.API.Models;
+
+namespace Rest.ASPNET.API.Services
+{
+    public interface IPersonService
+    {
+        Person FindById(long id);
+        List<Person> FindAll();
+        Person Create(Person person);
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
---
+++ Rest.ASPNET/Rest.ASPNET.API/Services/Implementations/PersonService.cs
@@ -0,0 +1,62 @@
+﻿using Rest.ASPNET.API.Models;
+
+namespace Rest.ASPNET.API.Services.Implementations
+{
+    public class PersonService : IPersonService
+    {
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> people = new List<Person>();
+            for(var i = 0; i < 6; i++)
+            {
+                Person person = MockPerson(i);
+                people.Add(person);
+            }
+
+            return people;
+        }        
+
+        public Person FindById(long id)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Matheus",
+                LastName = "Ribeiro",
+                Address = "Rua teste",
+                Gender = "Male"
+            };
+        }
+
+        public Person Update(Person person)
+            => person;
+        
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Person Name " + i,
+                LastName = "Person LastName " + i,
+                Address = "SomeAddress " + i,
+                Gender = "Male"
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+    }
+}
